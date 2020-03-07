e6874d2c5200e2e2dd32810ad0a276de28cf9339
Tonalli Rodriguez <tonalli@xanum.uam.mx>
2020-03-07 15:36:37 -0600

    Complete AddThing component

--- src/components/AddThing.js
+++ src/components/AddThing.js
@@ -5,6 +5,55 @@ import React, { Component } from 'react';
 import service from '../api/service';
 
 class AddThing extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      description: "",
+      imageUrl: ""
+    };
+  }
+
+  // this method submits the form
+  handleSubmit = event => {
+    event.preventDefault();
+
+    service.saveNewThing(this.state)
+      .then(response => {
+        console.log("added: ", response);
+        // here you would redirect to some other page
+      })
+      .catch(err => console.log("Error while adding the thing: ", err));
+  }
+  
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  // this method handles just the file upload
+  handleFileUpload = event => {
+    console.log("The file to be uploaded is: ", event.target.files[0]);
+
+    const uploadData = new FormData();
+    // imageUrl => this name has to be the same as in the 
+    // model since we pass req.body to .create() method when
+    // creating a new thing in '/api/things/create' POST route
+    uploadData.append("imageUrl", event.target.files[0]);
+
+    service.handleUpload(uploadData)
+      .then(response => {
+        // console.log('response is: ', response);
+        // after the console.log we can see that response carries 'secure_url' which we can use to update the state
+        this.setState({
+          imageUrl: response.secure_url
+        });
+      })
+      .catch(err => console.log("Error while uploading the file: ", err));
+  }
+  
   render() {
     return (
       <div>
